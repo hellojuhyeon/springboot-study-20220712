06e6df08cdf2c771f706c1de9b948d649976cc52
DESKTOP-NUOPUIF\ITPS <itps@google.com>
2022-07-12 20:51:33 +0900

    springbootStudy_1day

---
+++ src/main/java/com/springboot/studyjuhyeon/StudyJuhyeonApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjuhyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyJuhyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyJuhyeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/studyjuhyeon/StudyJuhyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studyjuhyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyJuhyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
